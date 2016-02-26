1229f22bdf58bef4bb1a53cfaf6ce473e73dc9d1
Ray Kootstra <r.kootstra@kootstradevelopment.nl>
2016-02-26 19:55:42 +0100

    Added attachments to the Mailer

--- Service/Mailer.php
+++ Service/Mailer.php
@@ -1,17 +1,36 @@
 <?php
 
+/*
+ * This file is part of the Mailer bundle from Stack Instance.
+ *
+ * (c) 2016 Ray Kootstra
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace StackInstance\MailerBundle\Service;
 
 /**
+ * Mailer service to send email via Swift Mailer in an easy way
+ *
  * Class Mailer
  * @package StackInstance\MailerBundle\Service
+ * @author Ray Kootstra <r.kootstra@stackinstance.com>
  */
 class Mailer
 {
+    const BODY_TYPE = 'text/html';
+
     /**
      * @var \Swift_Mailer
      */
     protected $mailer;
+
+    /**
+     * @var \Swift_Message
+     */
+    protected $message;
 
     /**
      * @param \Swift_Mailer $mailer
@@ -19,25 +38,53 @@ class Mailer
     public function __construct(\Swift_Mailer $mailer)
     {
         $this->mailer = $mailer;
+        $this->message = \Swift_Message::newInstance();
     }
 
     /**
-     * @param $subject
-     * @param $body
-     * @param $to
-     * @param $from
+     * @param string    $subject
+     * @param string    $body
+     * @param array     $to
+     * @param array     $from
+     * @param array     $attachments
      * @return $this
      */
-    public function send($subject, $body, $to, $from)
+    public function send($subject, $body, array $to, array $from, array $attachments)
     {
-        $message = \Swift_Message::newInstance();
-        $message->setSubject($subject);
-        $message->setFrom($from);
-        $message->setTo($to);
-        $message->setBody($body, 'text/html');
+        $this->message->setSubject($subject);
+        $this->message->setFrom($from);
+        $this->message->setTo($to);
+        $this->message->setBody($body, self::BODY_TYPE);
 
-        $this->mailer->send($message);
+        /** @var Attachment $attachment */
+        foreach($attachments as $attachment) {
+            if ($attachment instanceof Attachment) {
+                $this->attach($attachment);
+            }
+        }
 
+        $this->mailer->send($this->message);
+
+        return $this;
+    }
+
+    /**
+     * @param Attachment $attachment
+     * @return $this
+     */
+    public function attach(Attachment $attachment)
+    {
+        $mailAttachment = \Swift_Attachment::fromPath($attachment->getFile());
+
+        if ($attachment->getFilename() !== null) {
+            $mailAttachment->setFilename($attachment->getFilename());
+        }
+
+        if ($attachment->getMimeType() !== null) {
+            $mailAttachment->setContentType($attachment->getMimeType());
+        }
+
+        $this->message->attach($mailAttachment);
         return $this;
     }
 
