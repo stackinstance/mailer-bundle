7151a26b1adf7aa325e499c6ec0618205fb86075
Stack Instance <info@raymondkootstra.nl>
2020-09-30 14:26:33 +0200

    Update Mailer.php
    added addReplyTo when returnpath is set

--- Service/Mailer.php
+++ Service/Mailer.php
@@ -61,15 +61,18 @@ class Mailer implements MailerInterface
 
         if ($returnPath !== null) {
             $this->message->setReturnPath($returnPath);
+            $this->message->addReplyTo($returnPath);
+        } else {
+            if (is_array($from)) {
+                foreach($from as $address => $name) {
+                    $this->message->addReplyTo($address, $name);
+                }
+            } else {
+                $this->message->addReplyTo($from);
+            }
         }
 
-        if (is_array($from)) {
-            foreach($from as $address => $name) {
-                $this->message->addReplyTo($address, $name);
-            }
-        } else {
-            $this->message->addReplyTo($from);
-        }
+
         
         $this->message->setFrom($from);
 
