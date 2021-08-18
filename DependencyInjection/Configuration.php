65d5b0213ef8301d65a40a66d91a0ba677f040e6
Stack Instance <info@raymondkootstra.nl>
2021-08-18 13:22:46 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
          */
         public function getConfigTreeBuilder()
         {
-                $treeBuilder = new TreeBuilder();
-                $rootNode = $treeBuilder->root('stack_instance_mailer');
+                $treeBuilder = new TreeBuilder('stack_instance_mailer');
 
                 // Here you should define the parameters that are allowed to
                 // configure your bundle. See the documentation linked above for
